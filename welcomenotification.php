c8b279f80a08921db3827e3bb92e6c6b86ecddfb
gpaschos91 <gpaschos91@gmail.com>
2018-05-07 15:41:21 +0300

    server backup 2

---
+++ welcomenotification.php
@@ -0,0 +1,52 @@
+<?php
+
+require 'dbConfig.php';
+
+//Connection to Database
+$dbCon = new mysqli($dbip,$dbusername,$dbpass,$dbname);
+
+//Response Object
+$jObj = new stdClass();
+
+//Parse POST Variables
+if(isset($_POST['customerID']) && !empty($_POST['customerID'])){
+
+  $customerID = $_POST['customerID'];
+
+  $query = "SELECT COUNT(Occupancy.ID)
+            FROM Occupancy, Reservation
+            WHERE Occupancy.ReservationID=Reservation.ID AND Reservation.CustomerID=?";
+
+  $stmt = $dbCon->prepare($query);
+  $stmt->bind_param('i',$customerID);
+  $stmt->execute();
+  $stmt->bind_result($reservationIDCount);
+  $stmt->store_result();
+
+  $stmt->fetch();
+  if($reservationIDCount>0){
+    $jObj->hasStayed=true;
+  }
+  else{
+    $jObj->hasStayed=false;
+  }
+
+  $jObj->success=1;
+
+  //Close Connections
+  $stmt->close();
+  $dbCon->close();
+
+}
+//Email variable is not supplied
+else{
+  $jObj->success = 0;
+}
+
+//Encode data in JSON Format
+$JsonResponse = json_encode($jObj);
+
+//Show Data
+echo $JsonResponse;
+
+?>

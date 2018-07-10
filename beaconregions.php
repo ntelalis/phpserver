06fe82a682a512b520aa9ef52dce11b42d796a7a
cakewalker <cathm247@yahoo.com>
2018-07-10 13:21:37 +0300

    added exclusive regions funct. 2

--- beaconregions.php
+++ beaconregions.php
@@ -5,8 +5,6 @@ error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_STRICT);
 */
 
-//$_POST['regionsCheck'] = '[{"id":13,"modified":"2018-01-18 01:00:58"},{"id":16,"modified":"2019-01-18 01:00:58"},{"id":17,"modified":"2017-01-18 01:00:58"},{"id":18,"modified":"2019-01-18 01:00:58"}]';
-
 //Database connection variables
 include 'dbConfig.php';
 
@@ -15,15 +13,10 @@ $mysqli = new mysqli($dbip, $dbusername, $dbpass, $dbname);
 $mysqli->set_charset("utf8");
 
 
-<<<<<<< HEAD
   $query = "SELECT v.ID, v.UniqueID, v.UUID, v.Major, v.Minor, v.Modified, v.Exclusive
-            FROM BeaconRegionView v";
-=======
-  $query = "SELECT v.ID, v.UniqueID, v.UUID, v.Major, v.Minor, v.Modified
             FROM BeaconRegionView v
-            WHERE v.exclusive=0
             ORDER BY ID";
->>>>>>> c2620aac305ec098cd8eba5798e19b9137af2e1e
+
   $stmt = $mysqli->prepare($query);
   $stmt->execute();
   $stmt->bind_result($id, $uniqueID, $uuid, $major, $minor, $modified, $exclusive);

e4b66d04a3a51ad664ad4001dd103b178b655a58
George Paschos <ntelalis@gmail.com>
2018-12-16 21:40:39 +0200

    roomservicecategories and food rework

--- roomservicefood.php
+++ roomservicefood.php
@@ -1,65 +1,89 @@
 <?php
 
+//DEBUG
+/*
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_STRICT);
+*/
+
+//Database connection variables
 require 'dbConfig.php';
 
-
+//Create new database object
 $mysqli = new mysqli($dbip, $dbusername, $dbpass, $dbname);
 $mysqli->set_charset("utf8");
 
-$_POST['timeType'] = "Μεσημεριανό";
+//Response Object
+$jObj = new stdClass();
 
-if (isset($_POST['timeType'])) {
-    $timeType = $_POST['timeType'];
-    $jObj = new stdClass();
+//DEBUG
+//$_POST['menuTime'] = "Μεσημεριανό";
 
-    $query = "SELECT f.ID,f.Name,f.Description,f.Price,fmc.ID,fmc.Name
-  FROM Food f, FoodMenuTime fmt, FoodMenuCategory fmc, FoodMenuItem fmi
-  WHERE f.ID = fmi.FoodID AND fmt.ID = fmi.FoodMenuTimeID  AND fmc.ID = fmi.FoodMenuCategoryID
-  AND f.Availability = 1 AND fmt.Name = ?";
+if (isset($_POST['menuTime'])) {
 
+    $menuTime = $_POST['menuTime'];
+
+    //get all available foods for this menutime
+    $query = "  SELECT f.ID,f.Name,f.Description,f.Price,fmc.Name
+                FROM Food f, FoodMenuTime fmt, FoodMenuCategory fmc, FoodMenuItem fmi
+                WHERE f.ID = fmi.FoodID AND fmt.ID = fmi.FoodMenuTimeID  AND fmc.ID = fmi.FoodMenuCategoryID
+                AND f.Availability = 1 AND fmt.Name = ?";
     $stmt = $mysqli->prepare($query);
-    $stmt->bind_param('s', $timeType);
+    $stmt->bind_param('s', $menuTime);
     $stmt->execute();
-    $stmt->bind_result($foodID, $foodName, $foodDesc, $foodPrice, $categoryID, $categoryName);
+    $stmt->bind_result($id, $name, $desc, $price, $category);
     $stmt->store_result();
-    $finalarray = array();
-    $typeCategory = array();
 
     while ($stmt->fetch()) {
-        $categoryObj = new stdClass();
-
-        if (!isset(${$categoryName})) {
-            ${$categoryName} = array();
-            $typeCategory[] = $categoryName;
+        //if specific category doesn't exist as a varible
+        if (!isset(${$category})) {
+            //create a variable with the name of the category
+            ${$category} = array();
+            //add it to a helper string table containg all categories
+            $categoryNameArray[] = $category;
         }
 
-        $foodObj = new stdClass();
-        $foodObj->id = $foodID;
-        $foodObj->name = $foodName;
-        $foodObj->desc = $foodDesc;
-        $foodObj->price = $foodPrice;
-        ${$categoryName}[] = $foodObj;
-    }
-    $results = array();
-
-    for ($i=0;$i<count($typeCategory);$i++) {
-        $huh = new stdClass();
-        $huh->{$typeCategory[$i]} = ${$typeCategory[$i]};
-        $finalarray[] = $huh;
+        //create the food Object
+        $food = new stdClass();
+        $food->id = $id;
+        $food->name = $name;
+        $food->desc = $desc;
+        $food->price = $price;
+        //add it to the correct category array
+        ${$category}[] = $food;
     }
 
+    //create the response array which will be array of objects
+    $categoryArray = array();
+    //for all the categories that were found (find from the helper array)
+    foreach ($categoryNameArray as $categoryName) {
+        //create a new object
+        $category = new stdClass();
+        //this object has one field (which is named after the category)
+        //and its value is the corresponding array of objects (foods)
+        $category->{$categoryName} = ${$categoryName};
+        //$category->Κρύα ορεκτικα = $Κρύα ορεκτικά;
+        //add it to the response array
+        $categoryArray[] = $category;
+    }
+
+    //Close Connection to DB
     $stmt->close();
     $mysqli->close();
 
-    $jObj->success=1;
-    $jObj->typeCategory = $finalarray;
-} else {
-    $jObj->success=0;
-    $jObj->errorMessage=$mysqli->error;
+    //Build the json response
+    $jObj->categoryArray = $categoryArray;
+}
+//Bad request
+else {
+    $jObj->success = 0;
+    $jObj->errorMessage = "Bad Request";
 }
 
-//Encode data in JSON Format
-$JsonResponse = json_encode($jObj, JSON_UNESCAPED_UNICODE);
+//Specify that the response is json in the header
+header('Content-type:application/json;charset=utf-8');
 
-//Show Data
+//Encode the JSON Object and print the result
+$JsonResponse = json_encode($jObj, JSON_UNESCAPED_UNICODE);
 echo $JsonResponse;
